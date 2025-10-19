41d819ec94d02208eb4db5b9e3505bb7473ec514
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-19 23:43:01 +0500

    конструктор класса

--- Classes/Person.cs
+++ Classes/Person.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace L_R_1._2_Khasanova_BPI_23_01.Classes
 {
-    internal class Person
+    public class Person : IPersonFinance
     {
+        protected string fullName;
+        protected int age;
+        protected string gender;
+        protected double income;
+        protected double expense;
+
+        public Person(string fullName, int age, string gender, double income, double expense)
+        {
+            this.fullName = fullName;
+            this.age = age;
+            this.gender = gender;
+            this.income = income;
+            this.expense = expense;
+            
+        }
     }
 }
