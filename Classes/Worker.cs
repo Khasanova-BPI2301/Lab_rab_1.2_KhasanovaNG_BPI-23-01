0dcf08aeadc760004137a78540af9ba4d5d83bca
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-20 00:50:08 +0500

    создала конструктор который вызывает конструктор базового класса и переопределила

--- Classes/Worker.cs
+++ Classes/Worker.cs
@@ -8,5 +8,13 @@ namespace L_R_1._2_Khasanova_BPI_23_01.Classes
 {
     public class Worker : Person
     {
+        public Worker(string fullName, int age, string gender, double income, double expense)
+            : base(fullName, age, gender, income, expense)
+        {
+        }
+        public override string GetInfo()
+        {
+            return base.GetInfo() + " (Работающий)";
+        }
     }
 }
