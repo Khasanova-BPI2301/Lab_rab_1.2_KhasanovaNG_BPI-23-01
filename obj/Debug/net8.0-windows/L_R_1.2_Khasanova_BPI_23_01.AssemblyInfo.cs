ddf18c11e82848ab7898af500db6e8638df30860
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-20 00:47:33 +0500

    создала класс Worker

--- obj/Debug/net8.0-windows/L_R_1.2_Khasanova_BPI_23_01.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/L_R_1.2_Khasanova_BPI_23_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("L_R_1.2_Khasanova_BPI_23_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ac09ce7c176d28895a8af59b7df79762a1cca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49da683f7c3ce9c230fa30ea19088ce51d34251")]
 [assembly: System.Reflection.AssemblyProductAttribute("L_R_1.2_Khasanova_BPI_23_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("L_R_1.2_Khasanova_BPI_23_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
